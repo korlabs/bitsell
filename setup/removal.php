c5e43776f485a1fe1d104d94bb27cf4e1c68bc64
Lakota Lustig <lakota@podbe.at>
2014-08-28 01:51:14 -0400

    Update removal.php

--- setup/removal.php
+++ setup/removal.php
@@ -1,4 +1,40 @@
-<html> <head> <title> Setup Removal-BitSell </ title> <body>
+<!DOCTYPE HTML>
+<!--
+    Helios 1.5 by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        <title>BitSell-Setup Removal</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel="stylesheet" type="text/css" />
+        <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+        <script src="../js/jquery.min.js"></script>
+        <script src="../js/jquery.dropotron.min.js"></script>
+        <script src="../js/skel.min.js"></script>
+        <script src="../js/skel-panels.min.js"></script>
+        <script src="../js/init.js"></script>
+        <noscript>
+            <link rel="stylesheet" href="../css/skel-noscript.css" />
+            <link rel="stylesheet" href="../css/style.css" />
+            <link rel="stylesheet" href="../css/style-desktop.css" />
+            <link rel="stylesheet" href="../css/style-noscript.css" />
+        </noscript>
+        <!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+    </head>
+    <body class="homepage">
+
+        <!-- Header -->
+            <div id="header">
+                        
+                <!-- Inner -->
+                    <div class="inner">
+                        <header>
+                        
+<body>
 <?php
 /**
 * Deletes all files in the current folder and each subfolder, with the exception of script
@@ -22,7 +58,7 @@ echo '<p> Deleting file'. $ file-> getPathname (). </ p> ';
 }
 }
 } // foreach
-echo 'Deleting <h4 style="color:black;"> completed. There remains only the '. $ Script_name. " in the folder. 
+echo 'Deleting of setup files is  complete. All thats left is '. $ Script_name. " in the folder. 
 If you need to update anything, re-upload the files for the setup or manually configure the setup in /includes/configuration.php. 
 To start using your BitSell ATM, visit <?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/bitsellatm/.</ h4> ';
 ?>
