953e0f27580265ffa9a3301b45eb921f10bb0fe3
Lakota Lustig <lakota@podbe.at>
2014-08-28 14:32:32 -0400

    Update removal.php

--- setup/removal.php
+++ setup/removal.php
@@ -36,30 +36,21 @@
                         
 <body>
 <?php
-/**
-* Deletes all files in the current folder and each subfolder, with the exception of script
-* Based on a snippet: <a
-*/
-define ('DS', DIRECTORY_SEPARATOR);
-error_reporting (E_ALL);
-ini_set ('display_errors', 'On');
-$ script_name = "removal.php";
-$ directory = "setup";
-echo '<h1> Deleting all files and sub-folder'. $ directory. '</ h1>';
-$it = new RecursiveDirectoryIterator($directory);
-foreach (new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST) as $file) {
-if (($ file-> isDir ()) && (! in_array ($ file-> getFilename (), array ('.', '..')))) {
-echo '<h3> Deleting file'. $ file-> getPathname (). '</ h3>';
-@ rmdir ($ file-> getPathname ());
-} else {
-if (($ file-> isFile ()) && ($ file-> GetPathName ()! = __FILE__)) {
-echo '<p> Deleting file'. $ file-> getPathname (). </ p> ';
-@ unlink ($ file-> getPathname ());           
-}
-}
-} // foreach
-echo 'Deleting of setup files is  complete. All thats left is '. $ Script_name. " in the folder. 
-If you need to update anything, re-upload the files for the setup or manually configure the setup in /includes/configuration.php. 
-To start using your BitSell ATM, visit <?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/bitsellatm/.</ h4> ';
+<?php
+  function rrmdir($dir) {
+  if (is_dir($dir)) {
+    $objects = scandir($dir);
+    foreach ($objects as $object) {
+      if ($object != "." && $object != "..") {
+        if (filetype($dir."/".$object) == "dir") 
+           rrmdir($dir."/".$object); 
+        else unlink   ($dir."/".$object);
+      }
+    }
+    reset($objects);
+    rmdir($dir);
+  }
+ }
+?>
 ?>
 </body></html>
