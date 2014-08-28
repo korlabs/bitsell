b8088ffabf8649fd4079ca1199351e8ddd87327a
Lakota Lustig <lakota@podbe.at>
2014-08-27 22:34:49 -0400

    Removed values from configuration.php.
    The values already in there are used and won't do any harm to be there but they will interfere with the setup so... removed.

--- includes/configuration.php
+++ includes/configuration.php
@@ -2,24 +2,24 @@
 
 // BitSell Configuration File
 
-$lang = "english";
+$lang = "";
 
 
-$setup = "yes";
+$setup = "";
 
 
-$license = "bitsellufZkeSJ7bTOP4iJFpxpH9uq9v";
+$license = "";
 
 
-$api_key = "bitsellufZkeSJ7bTOP4iJFpxpH9uq9v";
+$api_key = "";
 
-$address = "bitsellatm.com";
+$address = "";
 
 // coinpay
 
-$coinpay_private = "072c5FCCd77F869A8d3972E79650e6ba97a2557F3Eb5a6BcF777D88E37af8564";
+$coinpay_private = "";
 
-$coinpay_public = "c6d3d715a8cf35ea94f9e30bf4ca156471d23af4179fad05a33b2c7f1b7e4423";
+$coinpay_public = "";
 
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
